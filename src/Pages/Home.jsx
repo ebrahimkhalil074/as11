2b251421f00a5d7b24e59288a4c0b5b3e46278c9
ebrahimkhalil074 <ebrahimkhalil074.com>
2023-11-08 19:21:46 +0600

    revew scc

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 
 import Newsletter from "../components/Newsletter";
 import RecentPosts from "../components/RecentPosts";
+import Revew from "../components/Revew";
 import Slider from "../components/Slidder";
 
 
@@ -10,6 +11,7 @@ const Home = () => {
              <Slider img={'https://i.ibb.co/NSPr4tQ/images-7.jpg'}img1={"https://i.ibb.co/6YS5DCN/download-13.jpg"} img2={"https://i.ibb.co/LCLqCPB/download-15.jpg"} img3={"https://i.ibb.co/zb4hwS8/how-to-learn-a-language-for-travel-7-steps.webp"} img4={"https://i.ibb.co/rvTK98V/download-20.jpg"} img5={"https://i.ibb.co/xYD9fZz/download-21.jpg"}></Slider> 
            <RecentPosts></RecentPosts>
            <Newsletter></Newsletter>
+           <Revew></Revew>
         </div>
     );
 };
---
+++ src/components/Revew.jsx
@@ -0,0 +1,33 @@
+
+
+const Revew = () => {
+    return (
+        <div>
+          <h1 className="text-4xl font-bold text-center"> Blog Review</h1>
+            <div className="flex gap-5 sm:flex-col">
+<div className="card bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={'https://i.ibb.co/9rqWwqS/116521839-800232863715149-6729636571760715521-n.jpg'} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+ <p>The quality of content is exceptional. Each post is well-written, engaging, and informative. The blog covers a diverse range of topics, catering to its target audience. The information presented is accurate, well-researched, and supported by relevant images, infographics, or videos, enriching the reading experience.</p>
+  </div>
+</div>
+
+<div className="card  bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={'https://i.ibb.co/1RhG12H/269623991-1104788393259593-2189958061777799806-n.jpg'} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+ <p>The quality of content is exceptional. Each post is well-written, engaging, and informative. The blog covers a diverse range of topics, catering to its target audience. The information presented is accurate, well-researched, and supported by relevant images, infographics, or videos, enriching the reading experience.</p>
+  </div>
+</div>
+
+
+
+        </div>
+        </div>
+    );
+};
+
+export default Revew;
